3293b64551a352814386773a40b286a801acafda
Adriase97 <adriase97@gmail.com>
2025-03-01 19:37:21 +0100

    Creatimg Unit Tests of Persistence

--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c479b123512e6a659bbc644e03e83e9bde1e1b3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
